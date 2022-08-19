3f836be9ecbb459f84807c552cc6de7e0668ad37
Hamza-777 <hamzararrani77@gmail.com>
2022-08-19 11:59:44 +0530

    Added a comment

--- prdclient.cs
+++ prdclient.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace LinqEg
 {
+    // A comment from Hamza Rarani
     class prdclient
     {
         public static List<Product> products = Product.getAllPrds();
